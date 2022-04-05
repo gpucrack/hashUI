ca082d794115f646249a97b3fc123b936374e909
Roxane Yvanes <roxaneyvanes@yahoo.fr>
2022-04-05 17:23:57 +0200

    a

--- client/src/App.js
+++ client/src/App.js
@@ -16,28 +16,26 @@ function App() {
     <div className="App">
       <Header/>
       <header className="App-header">
-      <div class="animated-title">
-       <div class="text-top">
-        <div>
-          <span>Welcome on</span>
+        <div class="animated-title">
+          <div class="text-top">
+            <div><span>Welcome on</span></div>
+          </div>
+          <div class="text-bottom">
+            <div>GPUCrack</div>
+          </div>
         </div>
-      </div>
-      <div class="text-bottom">
-        <div>GPUCrack</div>
+        <div className="welcome">
+          <NavLink to="/onehash"><button class="button-1" role="button" >One hash</button></NavLink>
+          <NavLink to="/result"><button class="button-2" role="button">Several hash</button></NavLink>
+        </div>
         
-       </div>
-
-    <div className="welcome">
-      <NavLink to="/onehash"><button class="button-1" role="button" >One hash</button></NavLink>
-      <NavLink to="/severalhash"><button class="button-2" role="button">Several hash</button></NavLink>
-    </div>
-  </div>
-</header>
-
+      </header>
     </div>
   );
 }
 
 export default App;
+
+
 
 
