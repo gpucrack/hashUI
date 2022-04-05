8efbe5085a004cb6235b95df22ef5e62ba08812e
Roxane Yvanes <roxaneyvanes@yahoo.fr>
2022-04-05 19:22:52 +0200

    pwd

--- client/src/Result.js
+++ client/src/Result.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './Result.css';
-import './OneHash'
+import './OneHash';
 
 import Header from "./Header";
 
@@ -8,16 +8,26 @@ function getPWD(){ //récupération du mot de passe
     return localStorage.getItem("pwd");
 }
 
+
 function Result() {
     var pwd = getPWD();
     return (
         <div className="App">
             <Header/>
         <header className="App-heade">
-            <h2>PASSWORD FOUND</h2>
+            <h1 class="PwdFound">
+                <span class="word">PASSWORD FOUND</span>
+            </h1>
             <p>The result is : </p>
-            {pwd}
+            <div className="Pwd">
+                {pwd}
+            </div>
+
+
+
+
         </header>
+
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 // server/index.js
 
-const PATH ='~/Documents/4INFO/Projet_GPU/hashUI/';
+const PATH ='~/Projet/hashUI/';
 
 const express = require("express");
 
