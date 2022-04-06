5b1fac7d73ea7d0eaa76044222f1f772799b43eb
Roxane Yvanes <roxaneyvanes@yahoo.fr>
2022-04-06 11:01:32 +0200

    stat

--- client/src/Result.js
+++ client/src/Result.js
@@ -28,6 +28,64 @@ function Result() {
 
         </header>
 
+
+
+        <section>
+            <div class="container2">
+                <div class="card">
+                    <div class="face face1">
+                        <div class="content">
+                            <div class="icon">
+                                <i>How long did it take to find the password?</i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <h3>
+                                Time :
+                            </h3>
+                            <p>tmps</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="face face1">
+                        <div class="content">
+                            <div class="icon">
+                                <i>How many false alarms have there been?</i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <h3>
+                            False alarms :
+                            </h3>
+                            <p>almrs</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="face face1">
+                        <div class="content">
+                            <div class="icon">
+                                <i>In which table was the password found?</i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <h3>
+                                Table :
+                            </h3>
+                            <p>tab</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
         </div>
     );
 }
