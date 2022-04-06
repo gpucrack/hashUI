66617f6a13da007caa9884c3fea49f6d12b8ff4c
hloussou <hugo.loussouarn@laposte.net>
2022-04-06 13:58:44 +0200

    update css

--- client/src/Result.js
+++ client/src/Result.js
@@ -22,12 +22,77 @@ function Result() {
         <div className="App">
             <Header/>
         <header className="App-heade">
-            <h2>PASSWORD FOUND</h2>
+            <h1 class="PwdFound">
+                <span class="word">PASSWORD FOUND</span>
+            </h1>
             <p>The result is : </p>
-            {pwd}
-            {time}
-            {alert}
+            <div className="Pwd">
+                {pwd}
+            </div>
+
+
+
+
         </header>
+
+
+
+        <section>
+            <div class="container2">
+                <div class="card">
+                    <div class="face face1">
+                        <div class="content">
+                            <div class="icon">
+                                <i>How long did it take to find the password?</i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <h3>
+                                Time :
+                            </h3>
+                            {time}
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="face face1">
+                        <div class="content">
+                            <div class="icon">
+                                <i>How many false alarms have there been?</i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <h3>
+                            False alarms :
+                            </h3>
+                            {alert}
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="face face1">
+                        <div class="content">
+                            <div class="icon">
+                                <i>In which table was the password found?</i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <h3>
+                                Table :
+                            </h3>
+                            <p>tab</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
         </div>
     );
 }
